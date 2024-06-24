7aaa5019e075867331507f54abd0c6e9fbb6ffe7
Ayushi1298 <116870125+Ayushi1298@users.noreply.github.com>
2024-06-25 02:55:30 +0530

    Add files via upload

---
+++ 5_calculator.py
@@ -0,0 +1,99 @@
+from tkinter import *
+
+r = Tk()
+r.geometry("520x500")
+r.title("Calculator")
+r.configure(bg="black")
+e = Entry(r, width=80, borderwidth=5)
+e.grid(row=0, column=0, columnspan=4, padx=10, pady=10)
+
+def onClick(num):
+    current=e.get()
+    e.delete(0, END)
+    e.insert(0,str(current)+str(num))
+def onClear():
+    e.delete(0,END)
+def but_ad():
+    fn=e.get()
+    global f,q
+    q='+'
+    f=int(fn)
+    e.delete(0,END)
+def but_sub():
+    fn=e.get()
+    global f,q
+    q='-'
+    f=int(fn)
+    e.delete(0,END)
+def but_mul():
+    fn=e.get()
+    global f,q
+    q='*'
+    f=int(fn)
+    e.delete(0,END)
+def but_div():
+    fn=e.get()
+    global f,q
+    q='/'
+    f=int(fn)
+    e.delete(0,END)
+def but_mod():
+    fn=e.get()
+    global f,q
+    q='%'
+    f=int(fn)
+    e.delete(0,END)
+def but_eq():
+    sn=e.get()
+    e.delete(0,END)
+    if(q=="+"):
+        e.insert(0,f+int(sn))
+    elif (q == "-"):
+        e.insert(0, f - int(sn))
+    elif (q == "*"):
+        e.insert(0, f * int(sn))
+    elif (q == "/"):
+        e.insert(0, f / int(sn))
+    elif (q == "%"):
+        e.insert(0, f % int(sn))
+button1 = Button(r, text="1", font=5, padx=40, pady=20, command= lambda: onClick(1))
+button2 = Button(r, text="2", font=5,padx=40, pady=20, command= lambda: onClick(2))
+button3 = Button(r, text="3", font=5,padx=40, pady=20, command= lambda: onClick(3))
+button4 = Button(r, text="4", font=5,padx=40, pady=20, command= lambda: onClick(4))
+button5 = Button(r, text="5", font=5,padx=40, pady=20, command= lambda: onClick(5))
+button6 = Button(r, text="6", font=5,padx=40, pady=20, command= lambda: onClick(6))
+button7 = Button(r, text="7", font=5,padx=40, pady=20, command= lambda: onClick(7))
+button8 = Button(r, text="8", font=5,padx=40, pady=20, command= lambda: onClick(8))
+button9 = Button(r, text="9", font=5,padx=40, pady=20, command= lambda: onClick(9))
+button0 = Button(r, text="0", font=5,padx=40, pady=20, command= lambda: onClick(0))
+
+button_add = Button(r, text="+",font=5, padx=39, pady=20, command= but_ad)
+button_sub = Button(r, text="-",font=5, padx=39, pady=20, command= but_sub)
+button_mul = Button(r, text="*",font=5, padx=39, pady=20, command= but_mul)
+button_div = Button(r, text="/",font=5, padx=39, pady=20, command= but_div)
+button_mod = Button(r, text="%",font=5, padx=34, pady=20, command= but_mod)
+button_eql = Button(r, text="=",font=5, padx=167, pady=20, command= but_eq)
+button_clr = Button(r, text="Clear",font=5, padx=88, pady=20,command= onClear)
+
+button0.grid(row=4,column=0,pady=5)
+button1.grid(row=3,column=0,pady=5)
+button2.grid(row=3,column=1,pady=5)
+button3.grid(row=3,column=2,pady=5)
+
+button4.grid(row=2,column=0,pady=5)
+button5.grid(row=2,column=1,pady=5)
+button6.grid(row=2,column=2,pady=5)
+
+button7.grid(row=1,column=0,pady=5)
+button8.grid(row=1,column=1,pady=5)
+button9.grid(row=1,column=2,pady=5)
+
+button_add.grid(row=1,column=3,pady=5)
+button_sub.grid(row=2,column=3,pady=5)
+button_mul.grid(row=3,column=3,pady=5)
+button_div.grid(row=4,column=3,pady=5)
+button_eql.grid(row=5,column=0,columnspan=3,pady=5)
+button_mod.grid(row=5,column=3,pady=5)
+button_clr.grid(row=4,column=1,columnspan=2)
+
+r.mainloop()
